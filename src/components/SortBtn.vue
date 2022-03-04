d01855211e447180c5e55615ace1eb70e4f8cd64
Rita Shinger <diskopauk@yandex.ru>
2022-03-04 17:38:10 +0300

    sort btn added

--- src/components/SortBtn.vue
+++ src/components/SortBtn.vue
@@ -1,26 +1,30 @@
 <template>
-  <label for="sort" class="hidden">Сортировать</label>
-  <select name="sort" id="sort" class="sort">
-    <option v-for="sort in sortList" :key="sort" value>{{ sort }}</option>
-  </select>
+	<label for="sort" class="hidden">Сортировать</label>
+	<select name="sort" id="sort" class="sort">
+		<option
+			v-for="sortItem in sortList"
+			:key="sortItem"
+			:value="sortItem"
+		>{{ sortItem }}</option>
+	</select>
 </template>
 
 <script setup>
-const sortList = ["По умолчанию", "По цене", "По названию"];
+const sortList = ["По цене min", "По цене max", "По наименованию"];
 </script>
 
 <style lang="scss" scoped>
 .sort {
-  margin-left: auto;
-  height: min-content;
-  align-self: end;
-  padding: $sm_r $nm_r;
-  background-color: $bg;
-  color: $disabled_text;
-  font-size: $md;
-  box-shadow: $elem_shadow;
-  @include mobile-only {
-    grid-row-start: 3;
-  }
+	margin-left: auto;
+	height: min-content;
+	align-self: end;
+	padding: $sm_r $nm_r;
+	background-color: $bg;
+	color: $disabled_text;
+	font-size: $md;
+	box-shadow: $elem_shadow;
+	@include mobile-only {
+		grid-row-start: 3;
+	}
 }
 </style>
